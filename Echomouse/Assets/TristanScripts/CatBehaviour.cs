969b241327f96b1a06e90e045bd0cbfc2a6b9d3b
Tristan210381 <210381@student.glu.nl>
2023-09-19 13:47:12 +0200

    Kat AI begin

--- Echomouse/Assets/TristanScripts/CatBehaviour.cs
+++ Echomouse/Assets/TristanScripts/CatBehaviour.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] CatStats catStats;
+
+    [SerializeField]protected CatFSM m_state;
+    private NavMeshAgent agent;
+    private Rigidbody rb;
+
+    [SerializeField]Transform[] targets; //array of targets for like sounds or the player
+    private Transform currentTarget;
+
+    protected enum CatFSM
     {
-        
+        Wander,
+        Investigate,
+        Chase,
+        Attack
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        m_state = CatFSM.Wander;
+    }
+
+    private void Update()
+    {
+        UpdateCatState();
+    }
+
+    private void UpdateCatState()
+    {
+        float distanceToTarget = Mathf.Infinity;
+
+        foreach (Transform target in targets)
+        {
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            if (distance < distanceToTarget)
+            {
+                distanceToTarget = distance;
+                currentTarget = target;
+            }
+        }
+
+        switch (m_state)
+        {
+            case CatFSM.Wander:
+                if (distanceToTarget <= catStats.catSenseRange)
+                    m_state = CatFSM.Chase;
+                break;
+        }
+
+        //switch (m_state)
+        //{
+        //    case ChildFSM.Chase:
+        //        if (isStunned == true)
+        //            TrasitionToStunnedState();
+        //        else if (distanceToTarget <= enemyStats.attackRange)
+        //            m_state = ChildFSM.Attack;
+        //        break;
+
+        //    case ChildFSM.Attack:
+        //        if (isStunned == true)
+        //            TrasitionToStunnedState();
+        //        else if (distanceToTarget > enemyStats.attackRange)
+        //            m_state = ChildFSM.Chase;
+        //        break;
+
+        //    case ChildFSM.Stunned:
+        //        if (isStunned == true)
+        //            return;
+        //        else if (distanceToTarget <= enemyStats.attackRange)
+        //            m_state = ChildFSM.Attack;
+        //        else if (distanceToTarget > enemyStats.attackRange)
+        //            m_state = ChildFSM.Chase;
+        //        break;
+        //}
     }
 }
---
+++ Echomouse/Assets/TristanScripts/CatStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCatStats",
+    menuName = "Cat Stats", order = 0)]
+
+public class CatStats : ScriptableObject
+{
+    public float moveSpeed;
+    public float catSenseRange;
+}
